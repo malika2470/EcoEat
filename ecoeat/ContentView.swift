d20f7f1795062d53721dd98708586fde101749be
malika2470 <malika2470@gmail.com>
2024-05-05 00:02:39 -0400

    Initial Commit

---
+++ ecoeat/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ecoeat
+//
+//  Created by Malika Sharma on 2024-05-05.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ecoeat/ecoeatApp.swift
@@ -0,0 +1,17 @@
+//
+//  ecoeatApp.swift
+//  ecoeat
+//
+//  Created by Malika Sharma on 2024-05-05.
+//
+
+import SwiftUI
+
+@main
+struct ecoeatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
